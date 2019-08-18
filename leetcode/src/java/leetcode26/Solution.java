dac6e75b7f6bb27e5533485ba7a04f9792088d17
superning <howard@yun.sk>
2019-08-18 19:35:29 +0800

    完成26删除排序数组中的重复项（笔记）

---
+++ leetcode/src/java/leetcode26/Solution.java
@@ -0,0 +1,37 @@
+package leetcode26;
+
+class  Solution
+    //官方题解
+{
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+}
+
+//class Solution {
+//    public int removeDuplicates(int[] nums) {
+//        // 需要计算出哪个元素需要向前移动，并判断需要向前移动多少
+//        int step = 0;
+//        for(int j=1; j<nums.length;j++) {
+//            if(nums[j]==nums[j-1]) {
+//                // 元素与上个元素相等，表示当前元素属于重复元素
+//                step++;
+//            } else {
+//                // 该元素是一个非重复元素
+//                nums[j-step] = nums[j];
+//            }
+//        }
+//        return nums.length-step;
+//    }
+//}
