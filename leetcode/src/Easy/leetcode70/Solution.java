9583fb7afe79fe5a8796e7cda5311dc4ed7d0718
superning <xining.l.se@gmail.com>
2019-12-28 21:48:04 +0800

    提交70题-爬梯子（斐波那契数列）

---
+++ leetcode/src/Easy/leetcode70/Solution.java
@@ -0,0 +1,21 @@
+package leetcode70;
+
+/**
+ * 正向递归斐波那契数列比起反向斐波那契数列要好得多
+ */
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+
+        } else {
+            int[] res = new int[n];
+            res[0] = 1;
+            res[1] = 2;
+            for (int i = 2; i < n; i++) {
+                res[i] = res[i - 1] + res[i - 2];
+            }
+            return res[n - 1];
+        }
+    }
+}
